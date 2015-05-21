0ed65bee3b117a73cf33c0a0576bc66d0c9fd6e2
lachtan <lachtan@fnet.cz>
2015-05-22 00:22:31 +0200

    pripraveno do ostreho provozu

--- BusMaster.cpp
+++ BusMaster.cpp
@@ -1,7 +1,3 @@
-/**
-* This is the main Blank Simple C++ Project program file.
-*/
-
 #include <stdio.h>
 #include <propeller.h>
 #include "BusMaster.h"
@@ -77,17 +73,103 @@ void leds_init()
 
 int main()
 {	
+	uart_frame pc_frame;
+	uart_frame master_frame;
+	uart_frame slave_frame;
+
+	uart_conf* master = &master_frame.conf;
+	uart_conf* slave = &slave_frame.conf;
+	uart_conf* pc = &pc_frame.conf;
+
+	uart_queue master_queue;
+	uart_queue slave_queue;
+	uart_queue pc_queue;
+
+	leds_init();
+	uart_init(master, FREQ / (1 * 115200), 0, 2, 3);
+	uart_init(slave, FREQ / (1 * 115200), 4, 6, 7);
+	uart_init(pc, FREQ / (2 * 115200), 8, 10, 11);
+
+	queue_init(master_queue);
+	queue_init(slave_queue);
+	queue_init(pc_queue);
+
+	extern unsigned int _load_start_uart_rx_cog[];
+	cognew(_load_start_uart_rx_cog, master);
+	extern unsigned int _load_start_uart_tx_cog[];
+	cognew(_load_start_uart_tx_cog, master);
+
+	extern unsigned int _load_start_uart_rx_cog[];
+	cognew(_load_start_uart_rx_cog, slave);
+	extern unsigned int _load_start_uart_tx_cog[];
+	cognew(_load_start_uart_tx_cog, slave);
+
+	extern unsigned int _load_start_uart_tx_cog[];
+	cognew(_load_start_uart_tx_cog, pc);
+
+	send(pc_queue, "START");
+
+	int data;
+	while (true)
+	{		
+		// cele kolecko se musi stihnout za dobu prichodu jednoho znaku
+		// 86.8us -> 6944 taktu, coz klidne nemusi stacit
+		// muzu to stale jeste rozdelit na 3 ruzna jadra
+		// nakonec by se to mereni celkem i hodilo :(
+
+		data = master->rx_data;
+		if (data >= 0)
+		{
+			master->rx_data = -1;
+			queue_put(slave_queue, data);
+			queue_put(pc_queue, 'M');
+			queue_put(pc_queue, data);
+		}
+
+		data = slave->rx_data;
+		if (data >= 0)
+		{
+			slave->rx_data = -1;
+			queue_put(master_queue, data);
+			queue_put(pc_queue, 'S');
+			queue_put(pc_queue, data);
+		}
+
+		// predpokladam, ze fronta je po vetsinu casu prazdna
+		// takze operace zabere malo casu (zkracene vyhdonocovani)
+
+		if (master_queue.length > 0 && master->tx_data < 0)
+		{
+			master->tx_data = queue_get(master_queue);
+		}
+
+		if (slave_queue.length > 0 && slave->tx_data < 0)
+		{
+			slave->tx_data = queue_get(slave_queue);
+		}
+
+		if (pc_queue.length > 0 && pc->tx_data < 0)
+		{
+			pc->tx_data = queue_get(pc_queue);
+		}
+	}
+
+	return 0;
+}
+
+int _main()
+{	
 	uart_frame frame;
 	uart_conf* conf = &frame.conf;
 	uart_queue queue;
 
 	leds_init();
-	uart_init(conf, FREQ / (1 * 115200), 0, 2, 16);
+	uart_init(conf, FREQ / (1 * 115200), 0, 2, 3);
 	queue_init(queue);
 
 	bit_set(DIRA, 23);
 	bit_set(OUTA, 23);
-	waitcnt(CNT + FREQ / 2);
+	waitcnt(CNT + FREQ / 10);
 	bit_clear(OUTA, 23);
 
 	extern unsigned int _load_start_uart_rx_cog[];
@@ -128,3 +210,4 @@ int main()
 
 	return 0;
 }
+
